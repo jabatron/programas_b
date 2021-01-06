fdc38d66d7396c481b583f2a459a3f22a1a97974
ja-compaq <ja@zdb.es>
2021-01-06 19:21:19 +0100

    todo funcionando

--- prog_b16/progb_16_jabaselga.py
+++ prog_b16/progb_16_jabaselga.py
@@ -54,10 +54,10 @@ def check_url (url):
         raise argparse.ArgumentTypeError(msg)
 
 def check_path (path):
-    r = os.path.isdir (ruta)
-    r_w = os.access (ruta, os.W_OK)
+    r = os.path.isdir (path)
+    r_w = os.access (path, os.W_OK)
     if r and r_w:
-        return r, r_w
+        return path
     else:
         msg=f"{path}: No es una ruta valida donde guardar los datos"
         raise argparse.ArgumentTypeError(msg)
@@ -65,34 +65,40 @@ def check_path (path):
 def comprobar_argumentos ():
     parser = argparse.ArgumentParser (description="Búsqueda en google", epilog="@jabaselga")
     parser.add_argument ("-u", "--url", metavar="http://github.com/usuario/...", help="URL del fichero en GitHub a descargare", type=check_url)
-    parser.add_argument ("-d", "--directorio", metavar="PATH", help="Ruta donde descarga el fichero")
-    parser.add_argument ("-r", "--repo", help="Numero de búsquedas")
-    parser.add_argument ("-v", "--verbose", help="Muestra mas detalle", type=int, choices=[1, 2, 3], default=2)
+    parser.add_argument ("-d", "--directorio", metavar="PATH", help="Ruta donde descarga el fichero '.' para directorio actual", type=check_path)
+    parser.add_argument ("-r", "--repo", help="Numero de búsquedas", action="store_true")
+    parser.add_argument ("-v", "--verbose", help="Muestra mas detalle", action="store_true")
        
     args = parser.parse_args() 
 
     return args 
 
-def comprobar_url (url):
+def comprobar_url (url, args):
     m = re.search ('^https://(www\.)?github.com', url)
     if not m:
         print ("No es una dirección de github -> 'https://github.com/usuario/...")
         sys.exit ()
     else:
+        if args.verbose:
+            print ("URL valida, pertenecea github.com")
         return url
 
-def check_ruta (ruta):
+def check_ruta (ruta, args):
     r = os.path.isdir (ruta)
     r_w = os.access (ruta, os.W_OK)
 
+    if args.verbose:
+        print ("Path para guardar ficheros permitido")
     return r, r_w
 
-def github_raw (url):
+def github_raw (url, args):
     rurl=url.replace("github.com","raw.githubusercontent.com")
     rurl=rurl.replace ("blob/","")
+    if args.verbose:
+        print ("Preparando ruta de descarga del fichero ...")
     return rurl
 
-def github_download_file(rurl, ruta=None):
+def github_download_file(rurl, args, ruta=None):
     r = get(rurl)
    
     if r.status_code == 200:
@@ -101,12 +107,16 @@ def github_download_file(rurl, ruta=None):
         fichero = open (ruta+"/"+filename, 'wb')
         fichero.write(r.content)
         fichero.close ()
+        if args.verbose:
+            print (F"Fichero {Fore.GREEN}{filename}{Fore.RESET} descargado")
     else:
         print ("URL erronea, compruebe USUARIO, REPO y FICHERO")
         sys.exit ()
 
-def descargar_repo (url, ruta):
+def descargar_repo (url, args, ruta):
     
+    if args.verbose:
+        print ("Descargando repositiorio completo ...")
     new_url, _= url.split("blob")
     page=get(new_url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -124,12 +134,13 @@ def descargar_repo (url, ruta):
     r = get(ruta_zip)
     with open(fullname, "wb") as zip_file:
         zip_file.write(r.content)
- 
-     
 
+    if args.verbose:
+        print (f"Repositorio {Fore.GREEN}{repo}{Fore.RESET} descargado en formato zip -> {fullname}")
+ 
 def main ():
     args = comprobar_argumentos()
-    if args.verbose == 3:
+    if args.verbose:
         print (f"{Fore.BLUE}________________________________________________________________________________")
         print ('Ejercicio b16. Descargas de archivos de github.')
         print ('@jabaselga')
@@ -140,37 +151,36 @@ def main ():
     if args.url:
         url = args.url
     else:
-        url = input ("Introduzca la URL del fichero a descargar: ")
-        url = comprobar_url(url)
+        url = input ("Introduzca la URL del fichero a descargar: ").strip()
+        url = comprobar_url(url, args)
     
-    url_raw = github_raw (url)
+    url_raw = github_raw (url, args)
     
 # Comprobamos DIRECTORIO
     if args.directorio:
         ruta = args.directorio
     else:
-        ruta = input ("Introduce la ruta para descargar el fichero. (VACIO para ruta actual): ")
-        if ruta.strip() == "":
+        ruta = input ("Introduce la ruta para descargar el fichero. (VACIO para ruta actual): ").strip()
+        if ruta == "":
             ruta = os.getcwd()
 
-    r, rw = check_ruta (ruta)
+    r, rw = check_ruta (ruta, args)
 
 # Descargar fichero y grabar
     if r and rw:
-        github_download_file (url_raw, ruta)
+        github_download_file (url_raw, args, ruta)
     else:
-        print ("Problemas para guardar el fichero")
+        print ("Problemas para guardar el fichero. Ruta erronea o sin permisos.")
         sys.exit ()
 
 # Descargar todo el repositorio
-
     if args.repo:
         todo = args.repo
     else:
         todo = input ("¿Quieres descargar todo el repositorio?")
 
     if todo:
-        descargar_repo (url, ruta)
+        descargar_repo (url, args, ruta)
 
 if __name__ == "__main__":
     main ()
